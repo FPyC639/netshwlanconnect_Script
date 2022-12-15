78f1864275755c170dbe60372c99164a757d291e
Jose M. Serra Jr <71533268+FPyC639@users.noreply.github.com>
2022-12-15 10:52:48 -0500

    Updated PWRWifi.ps1

--- PWRWifi.ps1
+++ PWRWifi.ps1
@@ -2,7 +2,7 @@
 $networks = New-Object System.Collections.Generic.Dictionary"[Int, String]"
 foreach($line in $File){
 $wifi = $line.split('.')
-$networks.add($wifi[0],$wifi[1
+$networks.add($wifi[0],$wifi[1])
 }
 $net_name = $networks.Get_Item($args[0]).trim()
 netsh wlan connect name=$net_name
